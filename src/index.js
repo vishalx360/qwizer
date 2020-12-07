f764e3a24148c4b63fb52d0ffe757eac84578fc7
Vishal Kumar <25267239+vishalx360@users.noreply.github.com>
2020-12-07 18:29:04 +0530

    update tailwind v2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./tailwind/tailwind.output.css";
+import "./tailwind/tailwind.css";
 ReactDOM.render(<App />, document.getElementById("root"));
